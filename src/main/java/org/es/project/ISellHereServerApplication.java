084d392edd4b545a36ae2bdab0429bf105d1786c
Wesley Silva <wesley.silva@ccc.ufcg.edu.br>
2017-01-31 14:32:21 -0300

    Commit of initial project

---
+++ src/main/java/org/es/project/ISellHereServerApplication.java
@@ -0,0 +1,12 @@
+package org.es.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISellHereServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISellHereServerApplication.class, args);
+	}
+}

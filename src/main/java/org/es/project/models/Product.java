dc14454ea35c79b8d79117affb0394d9a4e040a0
Hadrizia <hadrizia.santos@ccc.ufcg.edu.br>
2017-02-04 17:41:51 -0300

    Completed Product persistence

--- src/main/java/org/es/project/models/Product.java
+++ src/main/java/org/es/project/models/Product.java
@@ -4,14 +4,27 @@ import java.awt.Image;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
+@Entity
 public class Product {
+	@Id
+	@GeneratedValue
+	private Long id;
+	@ManyToOne(cascade = CascadeType.ALL)
 	private User creator;
+	@ManyToOne(cascade = CascadeType.ALL)
 	private PointOfSale pointOfSale;
 	private String name;
 	private String comment;
 	private double price;
 	private Image image;
+	@OneToMany(cascade = CascadeType.ALL)
 	private List<Evaluation> evaluations;
 	
 	
@@ -117,6 +130,10 @@ public class Product {
 
 	public void setImage(Image image) {
 		this.image = image;
+	}
+
+	public Long getId() {
+		return id;
 	}
 	
 	

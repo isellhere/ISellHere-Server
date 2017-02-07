6bf92b37e46d9ddee1b7d5f2f32f30c477c61d53
iaron <iaron@DESKTOP-B51RLNF.home>
2017-02-07 20:28:56 -0300

    Adjusted and completed UserController

--- src/main/java/org/es/project/controllers/UserController.java
+++ src/main/java/org/es/project/controllers/UserController.java
@@ -2,10 +2,14 @@ package org.es.project.controllers;
 
 import javax.servlet.ServletException;
 
+import org.es.project.beans.EditUserBean;
 import org.es.project.beans.RegistrationBean;
+import org.es.project.exceptions.ExceptionHandler;
+import org.es.project.exceptions.InvalidRegistrationBodyException;
 import org.es.project.exceptions.Validator;
 import org.es.project.models.User;
 import org.es.project.services.interfaces.UserService;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -27,10 +31,17 @@ public class UserController {
 			consumes = MediaType.APPLICATION_JSON_VALUE, 
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> createUser(@RequestBody RegistrationBean requestBody) throws ServletException{
-		//Falta try-catch e checagem de excecao
-		User newUser = new User(requestBody.getUsername(), requestBody.getEmail(), requestBody.getPassword());
-		userService.save(newUser);
-		return new ResponseEntity<>(newUser, HttpStatus.CREATED); // Vai ser http mesmo?
+		try{
+			ExceptionHandler.checkRegistrationBody(requestBody);
+			User newUser = new User(requestBody.getUsername(), requestBody.getEmail(), requestBody.getPassword());
+			userService.save(newUser);
+			return new ResponseEntity<>(newUser, HttpStatus.CREATED); // Vai ser http mesmo?
+			
+		} catch(InvalidRegistrationBodyException irbe){
+			throw new ServletException("An error has occurred: " +irbe.getMessage());
+		}catch(DataAccessException dae){
+			throw new ServletException("An error has occurred: " +dae.getMessage());
+		}
 	}
 	
 	@RequestMapping(value = "/get/{username}", 
@@ -47,6 +58,40 @@ public class UserController {
 		}
 		
 		return response;
+	}
+	
+	@RequestMapping(value = "/update", 
+			method = RequestMethod.PUT, 
+			consumes = MediaType.APPLICATION_JSON_VALUE, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> updateUser(@RequestBody User reqBody) {
+		User updatedUser = userService.update(reqBody);
+
+		if (updatedUser == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/edit", 
+			method = RequestMethod.PUT, 
+			consumes = MediaType.APPLICATION_JSON_VALUE, 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<User> editUser(@RequestBody EditUserBean reqBody) throws ServletException{
+		try{
+			ExceptionHandler.checkPassword(reqBody.getNewPassword());
+			User dbUser = userService.findByUsername(reqBody.getUser().getUsername());
+			dbUser.setPassword(reqBody.getNewPassword());
+			
+			User updatedUser = userService.update(dbUser);
+			
+			return new ResponseEntity<User>(updatedUser, HttpStatus.OK);
+		} catch(InvalidRegistrationBodyException irbe){
+			throw new ServletException("An error has occurred: " +irbe.getMessage());
+		} catch(DataAccessException dae){
+			throw new ServletException("An error has occurred: " +dae.getMessage());
+		}
 	}
 	
 	
--- src/main/java/org/es/project/exceptions/ExceptionHandler.java
+++ src/main/java/org/es/project/exceptions/ExceptionHandler.java
@@ -1,5 +1,26 @@
 package org.es.project.exceptions;
 
+import org.es.project.beans.RegistrationBean;
+
 public class ExceptionHandler {
+	
+	public static void checkRegistrationBody(RegistrationBean body){
+		if(Validator.isUsernameInvalid(body.getUsername())){
+			throw new InvalidRegistrationBodyException("The username " + body.getUsername() + " is not valid");
+		}
+		
+		if(Validator.isEmailInvalid(body.getEmail())){
+			throw new InvalidRegistrationBodyException("The E-mail " + body.getEmail() + " is not valid");
+		}
+		
+		checkPassword(body.getPassword());
+		
+	}
+	
+	public static void checkPassword(String password){
+		if(Validator.isPasswordInvalid(password)){
+			throw new InvalidRegistrationBodyException("Invalid Password");
+		}
+	}
 
 }

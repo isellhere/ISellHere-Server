b1db59a838fdd24ec1eefeaa3094ff90a2a01fd3
Iaron da Costa Araújo <iaron.araujo@ccc.ufcg.edu.br>
2017-03-11 14:02:07 -0300

    Small fix

--- src/main/java/org/es/project/util/tokens/HS512_24Hours_Token.java
+++ src/main/java/org/es/project/util/tokens/HS512_24Hours_Token.java
@@ -7,11 +7,10 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class HS512_24Hours_Token extends AbstractTokenBuilder{
-	public static long HOURS24_TO_MILLIS = 24 * 60 * 60 * 1000; 
+	public static final long HOURS24_TO_MILLIS = 24 * 60 * 60 * 1000; 
 	
 	public HS512_24Hours_Token() {
 		super(HOURS24_TO_MILLIS, SignatureAlgorithm.HS512, ServerConstants.TOKEN_KEY);
-		// TODO Auto-generated constructor stub
 	}
 
 }
